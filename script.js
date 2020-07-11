a6444afb6dd791c828eea3fb2e801300cb8c7df8
ArgiTavis <arigtaivs@gmail.com>
2020-07-10 23:31:32 -0400

    Adds a working 12 hour clock. Only seeable from console.

---
+++ script.js
@@ -0,0 +1,21 @@
+const makeDropDown = () => {
+    for (i = 0; i <= 11; i++){
+        console.log (1+i);
+    }
+}
+
+const makeTime = () => {
+    let today = new Date();
+    let hours = today.getHours() % 12 || 12;
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    console.log(hours + ':' + minutes + ':' + seconds);
+}
+
+const addTimeToDom = () =>{
+    
+}
+
+makeDropDown();
+makeTime();
