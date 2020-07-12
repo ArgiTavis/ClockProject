b85e368a91957f4d77164239cc28112af4999400
ArgiTavis <arigtaivs@gmail.com>
2020-07-11 22:40:33 -0500

    added new 12 hour clock with AM PM.

--- script.js
+++ script.js
@@ -4,18 +4,52 @@ const makeDropDown = () => {
     }
 }
 
-const makeTime = () => {
-    let today = new Date();
-    let hours = today.getHours() % 12 || 12;
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
+let today = new Date();
+let hours24 = today.getHours();
+let minutes = today.getMinutes();
+let seconds = today.getSeconds();
+let tod = 'AM';
+let hours12 = hours24;
 
-    console.log(hours + ':' + minutes + ':' + seconds);
+if (hours24 >= 12){
+    hours12 = hours24 - 12;
+    tod = 'PM'
+} 
+if (hours24 == 0) {
+    hours12 = 12;
 }
 
-const addTimeToDom = () =>{
+
+
+
+
+
+let select = document.getElementById('options');
+
+
+
+const testScript = () =>{
+    if (`${event.target.value}` == hours12){
+        alert('holy crap batman we did it!');
+    }
 
 }
 
-makeDropDown();
-makeTime();
+select.addEventListener("change", testScript);
+
+
+
+
+
+
+
+
+
+
+//
+// const addTimeToDom = () =>{
+
+// }
+
+// makeDropDown();
+// makeTime();
