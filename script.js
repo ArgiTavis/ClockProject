da5a99ccb0ec1f211aa1879bc129b9e677a48aee
ArgiTavis <48008726+ArgiTavis@users.noreply.github.com>
2020-08-31 13:46:27 -0500

    Add files via upload
    Refactors almost all the code.

--- script.js
+++ script.js
@@ -1,81 +1,89 @@
+let alarmTime = -1;
+
 const createTime = () => {
-    let today = new Date();
-    let hours24 = today.getHours();
+    const today = new Date();
+    const hours24 = today.getHours();
+    const hours12 = formatTime12(hours24);
+    const timeOfDay = getTimeOfDay(hours24);
     let minutes = today.getMinutes();
     let seconds = today.getSeconds();
-    let tod = 'AM';
-    let hours12 = hours24 % 12 || 12;
+
+    if (minutes < 10){
+        minutes = '0' + minutes;
+    }
     if (seconds < 10){
         seconds = '0' + seconds;
     }
-    if (hours24 >= 12){
-        tod = 'PM'
-    } 
-    if (hours24 == 0) {
-        hours12 = 12;
-    }
-    if (minutes < 10){
-        minutes = '0' + minutes
-    }
-    let time = {"hours12": hours12, "minutes": minutes, "seconds": seconds, "tod": tod, "hours24":hours24};
+
+    const time = {
+        "hours24":hours24,
+        "hours12": hours12, 
+        "timeOfDay": timeOfDay, 
+        "minutes": minutes, 
+        "seconds": seconds
+    };
+
     return time;
-    
-}
+};
 
-const makingArray = () =>{
-    let emptyArray = [];
-    for (let i = 0; i <= 23; i++) {
-        emptyArray.push(i);
-        
+const createNumberArray = (startValue, endValue) => {
+    let numberArray = [];
+    for (let i = startValue; i <= endValue; i++) {
+        numberArray.push(i);
     }
-    return emptyArray;
-}
 
-const addButtonsToDOM = (element) =>{
-    TOD = 'AM'
-    const select = document.getElementById('options');
-    const option = document.createElement('OPTION')
-    option.setAttribute('value',element)
-    if (element >= 12){
-        element = element - 12
-        TOD = 'PM'
-        }
-    if (element == 0){
-        element = 12
-    }
-    option.innerHTML = element + ' ' + TOD
-    select.appendChild(option)
+    return numberArray;
+};
 
-}
+const createDropdown = (timeOptions) => {
+    const optionsArray = timeOptions.map(getOptionElement);
+    optionsArray.forEach(addOptionsToDropdown);
+};
 
-const addTimeToDom = () =>{
+const getOptionElement = (timeValue) => {
+    const timeOfDay = getTimeOfDay(hours24);
+    const formattedTime = formatTime12(timeValue);
+
+    const option = document.createElement('OPTION');
+    option.setAttribute('value', formattedTime);
+    option.innerHTML = `${formattedTime} ${timeOfDay}`;
+
+    return option;
+};
+
+const addOptionsToDropdown = (optionElement) => {
+    const dropDown = document.getElementById('options');
+
+    dropDown.appendChild(optionElement);
+    dropDown.addEventListener('change', changeTimeSelected);
+};
+
+const updateTime = () => {
+    const timeElement = document.getElementById('time');
     const time = createTime();
-    const fullTime = time.hours12 + ':' + time.minutes + ':' + time.seconds + ' ' + time.tod;
-    const timeTag = document.getElementById('time');
-    timeTag.innerHTML = fullTime;
-    setInterval(addTimeToDom, 1000)
-}
+    const fullTime = `${time.hours12}:${time.minutes}:${time.seconds} ${time.timeOfDay}`;
 
-const createButtons = (array) => {
-    array.forEach(element => addButtonsToDOM(element));
-}
+    timeElement.innerHTML = fullTime;
 
-const checkForValue = () =>{
-    let time = createTime();
-    let timeSelected = 1;
-    let chosenTime = document.getElementById('options');
-    chosenTime.addEventListener('change', changeTimeSelected);
-            console.log(timeSelected)
-    }
+    if (alarmTime === time.hours24)  {
+        alert(`ALARM: ${fullTime}`);
+    }   
 
-const changeTimeSelected = () =>{
-    timeSelected = `${event.target.value}` 
-    if(timeSelected = time.hours24){
-        alert('Time')};}
+    setInterval(updateTime, 1000);
+};
 
-createButtons(makingArray());
-addTimeToDom();
-checkForValue();
+const changeTimeSelected = (event) => {
+    timeSelected = event.target.value;
+    alarmTime = timeSelected;
+};
+
+const getTimeOfDay = (time) => (time >= 12) ? 'AM' : 'PM';
+const formatTime12 = (time) => time % 12 || 12;
+
+(() => {
+    createDropdown(createArray(0, 23));
+    updateTime();
+})();
 
 
 
